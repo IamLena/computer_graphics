9e9c88251444e0359bc988597270f0b39c6af4c7
Lena <helly.luchina@gmail.com>
2019-02-17 22:34:21 +0300

    pretty input

--- lab_01/angle.py
+++ lab_01/angle.py
@@ -3,6 +3,24 @@ from math import sqrt
 
 root = Tk()
 
+#total
+def drawsys():
+    w.create_rectangle(5, 5, 600, 600)
+    w.create_line(5, 305, 600, 305)#x
+    w.create_line(305, 5, 305, 600)#y
+
+def error(message):
+    rect_label.grid_forget()
+    inputx_rect.grid_forget()
+    inputy_rect.grid_forget()
+    add_btn_rect.grid_forget()
+
+    error1.config(text= message)
+
+    error1.grid(row = 2, column = 2, columnspan = 3)
+    error2.grid(row = 3, column = 2, columnspan = 3)
+
+#rectangle
 def isrect(M):
     cx = (M[0][0] + M[1][0] + M[2][0] + M[3][0])/4
     cy = (M[0][1] + M[1][1] + M[2][1] + M[3][1])/4
@@ -20,22 +38,11 @@ def draw_rect(M):
     w.create_line(M[2][0], M[2][1], M[3][0], M[3][1])
     w.create_line(M[3][0], M[3][1], M[0][0], M[0][1])
 
-def error(message):
-    rect_label.grid_forget()
-    inputx.grid_forget()
-    inputy.grid_forget()
-    add_btn.grid_forget()
-
-    error1.config(text= message)
-
-    error1.grid(row = 2, column = 2, columnspan = 3)
-    error2.grid(row = 3, column = 2, columnspan = 3)
-
 def add_dot_rect(event):
-    x = inputx.get()
-    y = inputy.get()
-    inputx.delete(0,END)
-    inputy.delete(0,END)
+    x = inputx_rect.get()
+    y = inputy_rect.get()
+    inputx_rect.delete(0,END)
+    inputy_rect.delete(0,END)
     try:
         x = float(x)
         y = float(y)
@@ -54,18 +61,47 @@ def add_dot_rect(event):
         print(M)
         if (len(M) == 4):
             draw_rect(M)
+            rect_label.grid_forget()
+            inputx_rect.grid_forget()
+            inputy_rect.grid_forget()
+            add_btn_rect.grid_forget()
+
             if (not isrect(M)):
                 error('not a rectangle')
-                print('not a rectangle')
-                
+                print('not a rectangle')     
 
-#definition
-def add_dot(event):
-    x = inputx.get()
-    y = inputy.get()
+def createrect(event):
+    w.delete("all")
+    drawsys()
+
+    input_dots.grid_forget()
+
+    error1.grid_forget()
+    error2.grid_forget()
+
+    rect_label.grid(row = 2, column = 2, columnspan = 3)
+    inputx_rect.grid(row = 3, column = 2)
+    inputy_rect.grid(row = 3, column = 3)
+    add_btn_rect.grid(row = 3, column = 4)
+    input_dots.grid(row = 4, column = 2, columnspan = 3)
+
+    if (not (dots_label.grid_info() == {})):
+        dots_label.grid_forget()
+        inputx_dots.grid_forget()
+        inputy_dots.grid_forget()
+        add_btn_dots.grid_forget()
+        dots_label.grid(row=5, column=2, columnspan = 3)
+        inputx_dots.grid(row = 6, column = 2)
+        inputy_dots.grid(row = 6, column = 3)
+        add_btn_dots.grid(row = 6, column = 4)  
+
+#dots
+def add_dot_dots(event):
+    x = inputx_dots.get()
+    y = inputy_dots.get()
     print(x, y)
-    inputx.delete(0,END)
-    inputy.delete(0,END)
+    inputx_dots.delete(0,END)
+    inputy_dots.delete(0,END)
     try:
         x = float(x)
         y = float(y)
@@ -82,10 +118,12 @@ def add_dot(event):
             w.create_oval(x, y, x+1, y + 1, fill='black')
         print(M)
 
-def drawsys():
-    w.create_rectangle(5, 5, 600, 600)
-    w.create_line(5, 305, 600, 305)#x
-    w.create_line(305, 5, 305, 600)#y
+def inputdots(event):
+    row = input_dots.grid_info()['row']
+    dots_label.grid(row = row + 1, column = 2, columnspan = 3)
+    inputx_dots.grid(row = row + 2, column = 2)
+    inputy_dots.grid(row = row + 2, column = 3)
+    add_btn_dots.grid(row = row + 2, column = 4)
 
 def solve(event):
     drawsys()
@@ -102,49 +140,45 @@ def solve(event):
                     print('looking at this triangle:')
                     print(dot1,dot2,dot3)
 
-def createrect(event):
-    error1.grid_forget()
-    error2.grid_forget()
 
-    rect_label.grid(row = 2, column = 2, columnspan = 3)
-    inputx.grid(row = 3, column = 2)
-    inputy.grid(row = 3, column = 3)
-    add_btn.grid(row = 3, column = 4)
-    
 
 #buttons
-input_rect = Button(root, text = 'create rectangle', width = 20, height = 1, bg = 'black', fg = 'white')
+input_rect = Button(root, text = 'create new rectangle', width = 20, height = 1, bg = 'black', fg = 'white')
 rect_label = Label(root, text = 'input the coordinats of four dots\n in the correct order:')
 
-inputx = Entry(root, width = 10)
-inputy = Entry(root, width = 10)
+inputx_rect = Entry(root, width = 10)
+inputy_rect = Entry(root, width = 10)
+add_btn_rect = Button(root, text = 'add a dot', width = 8, height = 1, bg = 'black', fg = 'white')
+
+input_dots = Button(root, text = 'input dots', width = 20, height = 1, bg = 'black', fg = 'white')
+dots_label = Label(root, text = 'input the coordinats of dots:')
+
+inputx_dots = Entry(root, width = 10)
+inputy_dots = Entry(root, width = 10)
+add_btn_dots = Button(root, text = 'add a dot', width = 8, height = 1, bg = 'black', fg = 'white')
 
 error1 = Label(root, text = '', fg='red', font='bold')
 error2 = Label(root, text = 'try again', font='bold')
 
-add_btn = Button(root, text = 'add a dot', width = 8, height = 1, bg = 'black', fg = 'white')
 solve_btn = Button(root, text = 'solve', width = 20, height = 1, bg = 'black', fg = 'white')
 delete_btn = Button(root, text = 'clear', width = 8, height = 1, bg = 'black', fg = 'white')
+
 w = Canvas(root, width = 610, height = 610)
 
 #packer
 w.grid(row = 1, column = 1, rowspan = 20)
-
 input_rect.grid(row = 1, column = 2, columnspan = 3)
-
-# inputx.grid(row = 2, column = 6)
-# inputy.grid(row = 2, column = 7)
-# add_btn.grid(row = 2, column = 8)
-#solve_btn.grid(row = 4,column = 6, columnspan = 3)
-#delete_btn.grid(row = 2, column = 8)
-
+input_dots.grid(row = 2, column = 2, columnspan = 3)
 
 #run
 M =[]
 drawsys()
 input_rect.bind("<Button-1>", createrect)
+input_dots.bind("<Button-1>", inputdots)
 
-add_btn.bind("<Button-1>", add_dot_rect)
+add_btn_rect.bind("<Button-1>", add_dot_rect)
+add_btn_dots.bind("<Button-1>", add_dot_dots)
+
 #left click
 #add_btn.bind("<Return>", add_dot)#fires by enter
 # solve_btn.bind('<Button-1>', solve)
