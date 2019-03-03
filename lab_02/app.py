1377826292290541a921e21a4cdee39d8aa5b357
Lena <helly.luchina@gmail.com>
2019-03-03 19:11:24 +0300

    hashing vertical and empty epi

--- lab_02/app.py
+++ lab_02/app.py
@@ -10,6 +10,27 @@ from math import *
 def scale(dot):
     return [canvasWidth/2 + dot[0] * 12, canvasHeight/2 - dot[1] * 12]
 
+def fill_epic():
+    f = open("epicycloid.csv", "r")
+    fileCtx = (f.read())
+    lines = fileCtx.split('\n')
+    epic = []
+    for i in lines:
+        dot = i.split(',')
+        try:
+            x = float(dot[0])
+            y = float(dot[1])
+        except:
+            print('invalid file data')
+            return
+        else:
+            epic.append([x, y])
+    #print(epic)
+    for i in range(len(epic) - 1):
+        w.create_line(scale(epic[i]), scale(epic[i+1]))
+        w.create_polygon(scale(epic[i]), scale(epic[i+1]), scale([0, 0]), fill = 'white')
+        f.close()
+
 def draw_epic():
     f = open("epicycloid.csv", "r")
     fileCtx = (f.read())
@@ -28,9 +49,49 @@ def draw_epic():
     #print(epic)
     for i in range(len(epic) - 1):
         w.create_line(scale(epic[i]), scale(epic[i+1]))
+    f.close()
 
 def draw_rect():
     w.create_rectangle(scale([-15, 8]), scale([15, -8]))
+
+def hash_rect(left_up_corner, right_down_corner, step, angle):
+    i = left_up_corner[0]
+    print(i)
+    print(right_down_corner[0])
+    while (i < right_down_corner[0]):
+    #while (i < 200):
+        w.create_line(scale([i, left_up_corner[1]]), scale([i,  right_down_corner[1]]))
+        i += 1
+
+        # print(i)
+        # print(scale([i, left_up_corner[1]]))
+        # print(scale([i, right_down_corner[1]]))
+        # w.create_line(scale([i, left_up_corner[1]]), scale([i, right_down_corner[1]]))
+        # i += step
+
+    # i = left_up_corner
+    # print('hey')
+    # print(i[0], right_down_corner[0], i[1], right_down_corner[1])
+    # while (i[0] < right_down_corner[0] and i[1] < right_down_corner[1]):
+    #     print(i[0], right_down_corner[1], i[1], right_down_corner[0])
+    #     w.create_line(scale([i[0], right_down_corner[1]]), scale([i[1], right_down_corner[0]]))
+    #     i[0] += step
+    #     i[1] -= step
+
+    # height = abs(right_down_corner[0] - left_up_corner[0])
+    # width = abs(right_down_corner[1] - left_up_corner[1])
+    # step_d = sqrt(step**2 + step**2)
+    # diagonal = sqrt(height**2 + width**2)
+    # # diagonal = floor(diagonal)
+    # width_run = left_up_corner[0]
+    # height_run = left_up_corner[1]
+    # i = 0
+    # while (i < diagonal):
+    #     w.create_line(scale([width_run, left_up_corner[1]]), scale([height_run, left_up_corner[0]]))
+    #     width_run += step
+    #     height_run -= step
+    #     i += step_d
+
 
 def move(event):
     dx_lab.grid(row = 2, column = 11)
@@ -160,7 +221,10 @@ back = Button(root, width = 20, height = 1, text = "previous", font = '30', bg =
 back.grid(row = 12, column = 11, columnspan = 4)
 
 #run
-draw_epic()
+
 draw_rect()
+hash_rect([-15, 8], [15, -8], 45, 4)
+fill_epic()
+draw_epic()
 
 root.mainloop()
