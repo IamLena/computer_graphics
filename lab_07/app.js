a206c2972504904dc75b935ba6baa86a13b1fc9c
Lena <helly.luchina@gmail.com>
2019-05-14 23:04:43 +0300

    works, needs optimize

--- lab_07/app.js
+++ lab_07/app.js
@@ -32,6 +32,7 @@ const xHolder = document.querySelector('#x')
 const yHolder = document.querySelector('#y')
 const aHolder = document.querySelector('#a')
 const bHolder = document.querySelector('#b')
+
 let rectangle = []
 
 const lineColor = document.querySelector('#lineColor')
@@ -44,7 +45,11 @@ let lines = []
 
 const cutColor = document.querySelector('#cutColor')
 
-document.querySelector('#clean').addEventListener('click', (e) => ctx.clearRect(0, 0, width, height))
+document.querySelector('#clean').addEventListener('click', (e) => {
+    ctx.clearRect(0, 0, width, height)
+    rectangle = []
+    lines = []
+})
 
 document.querySelector('#drawRect').addEventListener('click', (e) => {
     ctx.strokeStyle = rectColor.value
@@ -88,15 +93,24 @@ document.querySelector('#cut').addEventListener('click', (e) => {
     ctx.strokeStyle = cutColor.value
     lines.forEach((line) => {
         const lineCode = inOut(line)
-        if (lineCode == 0) {
+        if (lineCode == 0) {//inside
             ctx.beginPath()
             ctx.moveTo(line[0], line[1])
             ctx.lineTo(line[2], line[3])
             ctx.stroke()
             ctx.closePath()
         }
-        else if (lineCode == 1) {
+        else if (lineCode == 1) {//cut
             console.log('culc cut')
+            findCut(line)
+            ctx.beginPath()
+            ctx.moveTo(line[0], line[1])
+            ctx.lineTo(line[2], line[3])
+            ctx.stroke()
+            ctx.closePath()
+        }
+        else {
+            console.log('out')//outside
         }
     })
 })
@@ -106,6 +120,15 @@ function convertToInt(x) {
         return NaN
     }
     else return parseInt(x)
+}
+
+function getCode(x, y, left, right, top, bottom) {
+    let code = 0
+    if (y > top) {code += 8}
+    if (y < bottom) {code += 4}
+    if (x > right) {code += 2}
+    if (x < left) {code += 1}
+    return code
 }
 
 function inOut(line) {
@@ -119,23 +142,78 @@ function inOut(line) {
     const right = left + rectangle[2]
     const top = bottom + rectangle[3]
 
-    let code1 = 0
-    if (y1 > top) {code1 += 8}
-    if (y1 < bottom) {code1 += 4}
-    if (x1 > right) {code1 += 2}
-    if (x1 < left) {code1 += 1}
+    let code1 = getCode(x1, y1, left, right, top, bottom)
+    let code2 = getCode(x2, y2, left, right, top, bottom)
 
-    let code2 = 0
-    if (y2 > top) {code2 += 8}
-    if (y2 < bottom) {code2 += 4}
-    if (x2 > right) {code2 += 2}
-    if (x2 < left) {code2 += 1}
+    console.log(code1, code2)
 
-    if (code1 == code2 && code1 == 0) {
-        return 0
+    if ((code1 == code2) && (code1 == 0)) {
+        return 0 // inside
     }
-    if (code1 & code2 != 0) {
-        return 1
+    if ((code1 & code2) != 0) {
+        return 2 //outside
     }
-    return 2
+    return 1 //cut
+}
+
+function culcLine(x1, y1, x2, y2) {
+    const m = (y1 - y2) / (x1 - x2)
+    const b = y1 - m * x1
+    return [m, b]
+}
+
+function findCut(line) {
+    let x1 = line[0]
+    let y1 = line[1]
+    let x2 = line[2]
+    let y2 = line[3]
+    const mb = culcLine(x1, y1, x2, y2)
+    const m = mb[0]
+    const b = mb[1]
+
+    const left = rectangle[0]
+    const bottom = rectangle[1]
+    const right = left + rectangle[2]
+    const top = bottom + rectangle[3]
+
+    let code = getCode(x1, y1, left, right, top, bottom)
+    if ((code & 8) === 8) {
+        x1 = (top - b ) / m
+        y1 = top
+    }
+    if ((code & 4) === 4) {
+        x1 = (bottom - b) / m
+        y1 = bottom
+    }
+    if ((code & 2) === 2) {
+        y1 = m * right + b
+        x1 = right
+    }
+    if ((code & 1) === 1) {
+        y1 = m * left + b
+        x1 = left
+    }
+
+    code = getCode(x2, y2, left, right, top, bottom)
+    if ((code & 8) === 8) {
+        x2 = (top - b ) / m
+        y2 = top
+    }
+    if ((code & 4) === 4) {
+        x2 = (bottom - b) / m
+        y2 = bottom
+    }
+    if ((code & 2) === 2) {
+        y2 = m * right + b
+        x2 = right
+    }
+    if ((code & 1) === 1) {
+        y2 = m * left + b
+        x2 = left
+    }
+
+    line[0] = x1
+    line[1] = y1
+    line[2] = x2
+    line[3] = y2
 }
