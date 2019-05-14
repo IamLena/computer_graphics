e4706db006aaa1c8ab2ec6ef07569b3a98191418
Lena <helly.luchina@gmail.com>
2019-05-14 20:54:42 +0300

    rect and lines drawing

--- lab_07/app.js
+++ lab_07/app.js
@@ -21,3 +21,74 @@ colorPickcut.onchange = function() {
     cutColor = colorPickcut.value
 }
 wrappercut.style.backgroundColor = colorPickcut.value
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const width = canvas.width
+const height = canvas.height
+
+const rectColor = document.querySelector('#rectColor')
+const xHolder = document.querySelector('#x')
+const yHolder = document.querySelector('#y')
+const aHolder = document.querySelector('#a')
+const bHolder = document.querySelector('#b')
+
+const lineColor = document.querySelector('#lineColor')
+const x1Holder = document.querySelector('#x1')
+const y1Holder = document.querySelector('#y1')
+const x2Holder = document.querySelector('#x2')
+const y2Holder = document.querySelector('#y2')
+
+const cutColor = document.querySelector('#cutColor')
+
+document.querySelector('#clean').addEventListener('click', (e) => ctx.clearRect(0, 0, width, height))
+
+document.querySelector('#drawRect').addEventListener('click', (e) => {
+    ctx.strokeStyle = rectColor.value
+    console.log(rectColor.value)
+    let x = convertToInt(xHolder.value)
+    let y = convertToInt(yHolder.value)
+    let a = convertToInt(aHolder.value)
+    let b = convertToInt(bHolder.value)
+    if (x && y && a && b) {
+        console.log(x, y, a, b)
+        ctx.beginPath()
+        ctx.rect(x, y, a, b)
+        ctx.stroke()
+        ctx.closePath()
+    }
+    else {
+        alert('input error')
+    }
+})
+
+document.querySelector('#drawLine').addEventListener('click', (e) => {
+    ctx.strokeStyle = lineColor.value
+    let x1 = convertToInt(x1Holder.value)
+    let y1 = convertToInt(y1Holder.value)
+    let x2 = convertToInt(x2Holder.value)
+    let y2 = convertToInt(y2Holder.value)
+    if (x1 && y1 && x2 && y2) {
+        ctx.beginPath()
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.stroke()
+        ctx.closePath()
+        console.log(x1, y1, x2, y2)
+    }
+    else {
+        alert('input error')
+    }    
+})
+
+document.querySelector('#cut').addEventListener('click', (e) => {
+    ctx.strokeStyle = cutColor.value
+    console.log('cut')
+})
+
+function convertToInt(x) {
+    if (isNaN(x) || x == '') {
+        return NaN
+    }
+    else return parseInt(x)
+}
